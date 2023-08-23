f157236fa6705846017cece464895e15978e070b
gwadson <68423815+gwadson@users.noreply.github.com>
2023-08-23 18:04:13 +0200

    Add files via upload

---
+++ analyses/StoreProfitsWithPreferredCustomersPerZip.sql
@@ -0,0 +1,34 @@
+
+-- This code provides a list of store names, 
+-- their zip codes, and their total net profits. 
+-- Additionally, it provides insights about a specific set of customers, 
+-- namely the preferred customers who reside in certain zip code. 
+
+with
+preferred_customers_by_zipcode as (
+    select
+        substr(ca_zip, 1, 5) as ca_zip
+        ,count(*) as cnt
+    from {{ ref("customers") }} as customers
+
+    inner join {{ref("zip_codes")}} as zip_codes on zip_codes.zip = customers.ca_zip
+    where c_is_preferred_customer = true
+    
+    group by customers.ca_zip
+)
+select 
+    stores.s_store_name
+    ,stores.s_zip
+    ,sum(store_sales.ss_net_profit) as store_total_net_profit
+from {{ ref("store_sales") }}
+inner join
+    {{ ref("dim_date") }} as dates
+    on dates.d_date_sk = store_sales.ss_sold_date_sk
+inner join
+    {{ ref("dim_store") }} as stores
+    on stores.s_store_sk = store_sales.ss_store_sk
+
+where dates.d_qoy = 2 and dates.d_year = 1998
+
+group by stores.s_store_name,stores.s_zip
+order by stores.s_zip,stores.s_store_name
